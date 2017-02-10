e79cda5355242d1dd20cb52e514fdd60c6e45bdf
mohamad-amin <mohamadamin@aut.ac.ir>
2017-02-11 02:28:10 +0330

    Added recycler view and findViewById

--- presentation-layer/src/main/java/com/mohamadamin/learningkotlin/MainActivity.kt
+++ presentation-layer/src/main/java/com/mohamadamin/learningkotlin/MainActivity.kt
@@ -2,14 +2,19 @@ package com.mohamadamin.learningkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello Kotlin:D"
+
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
+
     }
 
 }
